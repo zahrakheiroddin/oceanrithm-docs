9f3ee653504e72809de8b3922657531271642173
zahrakheiroddin <zahrakheiroddin1997@gmail.com>
2025-11-27 04:44:40 +0100

    Deployed 84f986d with MkDocs version: 1.6.1

---
+++ assets/js/schema-logo.js
@@ -0,0 +1,9 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "OceanRithm Solutions",
+  "url": "https://oceanrithm.com/",
+  "logo": "https://oceanrithm.com/assets/logo/oceanrithm-logo.png"
+}
+</script>
---
+++ assets/js/schema.js
@@ -0,0 +1,22 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "OceanRithm Solutions UG (haftungsbeschränkt)",
+  "legalName": "OceanRithm Solutions UG (haftungsbeschränkt)",
+  "url": "https://oceanrithm.com",
+  "logo": "https://oceanrithm.com/assets/logo/oceanrithm-logo.png",
+  "foundingDate": "2025",
+  "founder": {
+    "@type": "Person",
+    "name": "Zahra Kheiroddin"
+  },
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Schlieperstraße 78",
+    "addressLocality": "Berlin",
+    "postalCode": "13507",
+    "addressCountry": "DE"
+  }
+}
+</script>
