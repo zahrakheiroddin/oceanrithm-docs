aae385691737998c04b25515b81c8320bf46085a
zahrakheiroddin <zahrakheiroddin1997@gmail.com>
2025-11-27 05:54:16 +0100

    Deployed 84f986d with MkDocs version: 1.6.1

--- assets/js/schema-logo.js
+++ assets/js/schema-logo.js
@@ -1,9 +1,13 @@
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "OceanRithm Solutions",
-  "url": "https://oceanrithm.com/",
-  "logo": "https://oceanrithm.com/assets/logo/oceanrithm-logo.png"
-}
-</script>
+// docs/assets/js/schema-logo.js
+(function() {
+  var script = document.createElement('script');
+  script.type = 'application/ld+json';
+  script.text = JSON.stringify({
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "OceanRithm Solutions UG (haftungsbeschränkt)",
+    "url": "https://oceanrithm.com",
+    "logo": "https://oceanrithm.com/assets/logo/oceanrithm-logo.png"
+  });
+  document.head.appendChild(script);
+})();
